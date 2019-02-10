a20d2c006c5454ade99f2f22e570155d20c8a712
Henry Wu <brive3571@gmail.com>
2019-02-11 02:33:50 +0800

    [UPDATE] 0001 two sum python file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Leetcode practice and solution keyword
+### Leetcode practice and solution (inspired by [qiyuangong leetcode](https://github.com/qiyuangong/leetcode))
+♥ means you need a subscription
+
+| No. | Title | Code | Basic idea | Level |
+| -------- | -------- | -------- | ---|
+| 1     | [Two Sum](https://leetcode.com/problems/two-sum/)     | [Python](https://github.com/Anch9999/leetcode/python/0001_two_sum.py)  | Hashmap| Easy |
---
+++ python/0001_two_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for cnt, num in enumerate(nums):
+            if target - num in lookup:
+                return lookup[target-num], cnt
+            lookup[num] = cnt
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print(s.twoSum([2,7,11,15], 9))
